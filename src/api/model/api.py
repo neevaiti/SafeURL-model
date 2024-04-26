2c5cfbeb42e267b0d3658634248bc8d953bb3ed7
neevaiti <bjornstein-dev@pm.me>
2024-04-26 12:17:38 +0200

    🌟feat: model api done

--- src/api/model/api.py
+++ src/api/model/api.py
@@ -1,40 +1,56 @@
 from fastapi import FastAPI, HTTPException, Request
 import pickle
 import pandas as pd
+import os
 from io import StringIO
+from dotenv import load_dotenv
 
+from scripts.read_result import read_result
+
+
+load_dotenv()
 
 app = FastAPI()
 
+
+API_MODEL_KEY = os.getenv("API_MODEL_KEY")
+
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
-    
 
-def read_result(prediction):
+    
+@app.middleware("http")
+async def verify_api_key(request: Request, call_next):
     """
-    Function to interpret the prediction and return the corresponding response.
-    Takes a prediction as input and returns a string response.
+    A middleware function that verifies the API key in the request headers.
+    
+    Parameters:
+        request (Request): The incoming HTTP request.
+        call_next (callable): The callback function to proceed with the request handling.
+    
+    Returns:
+        The HTTP response after verifying the API key.
     """
-    if int(prediction[0]) == 0:
-        response="Bénin"
-        return response
-    elif int(prediction[0]) == 1:
-        response="Scam"
-        return response
-    elif int(prediction[0]) == 2:
-        response="Phishing"
-        return response
-    elif int(prediction[0]) == 3:
-        response="Malware"
-        return response
+    api_key = request.headers.get("X-API-Key")
+    if api_key != API_MODEL_KEY:
+        raise HTTPException(status_code=401, detail="Accès non autorisé")
+    response = await call_next(request)
+    return response
+
 
 @app.post("/predict/")
 async def predict(request: Request):
+    """
+    Asynchronous function to handle POST requests for prediction. 
+    Takes a Request object as input. 
+    Returns a dictionary containing the prediction result.
+    """
     csv_body = await request.body()
     csv_string = csv_body.decode('utf-8')
     features_df = pd.read_csv(StringIO(csv_string))
+    reading_columns = features_df.columns
+    print(reading_columns)
     prediction = model.predict(features_df)
     prediction = read_result(prediction)
-    
     
     return {"prediction": prediction}
