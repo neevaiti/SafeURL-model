3b9cef8b5665def21bd89fea258847053bd5988b
neevaiti <bjornstein-dev@pm.me>
2024-10-07 07:01:09 +0200

    🛠️refactor: changes build process

--- setup/azure_database/adding_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source .env
-
-
-#### ADD DATA TO List_url ####
-listurlcsv="/Users/ant/Desktop/Projects/SafeURL-model/dataset/df_1000.csv"
-psql -h "$SERVER_ADRESS" -d "$DB_NAME" -U "$ADMIN_USER" -c"\copy List_url FROM '$listurlcsv' DELIMITER ',' CSV HEADER;"
